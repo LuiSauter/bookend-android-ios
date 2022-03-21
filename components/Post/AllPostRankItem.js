1155e293705cf1eb555b0466dee59fa3b7fe1e3c
Luis Gabriel <janco7249@gmail.com>
2022-03-21 19:56:41 -0400

    added all post ranking

---
+++ components/Post/AllPostRankItem.js
@@ -0,0 +1,103 @@
+import React, { memo } from 'react'
+import { Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native'
+import { useQuery } from '@apollo/client'
+import { useNavigation, useTheme } from '@react-navigation/native'
+
+import { colorsRandom } from '../../config/colors'
+import BtnLike from '../Button/BtnLike'
+import useTimeAgo from '../../hooks/useTimeAgo'
+import { FIND_USER_BY_USER } from '../../user/graphql-queries'
+
+const AllPostRankItem = ({
+  bookUrl,
+  createdAt,
+  image,
+  title,
+  comments,
+  description,
+  id,
+  likes,
+  user,
+  author,
+}) => {
+  const { colors } = useTheme()
+  const navigation = useNavigation()
+  const date = Number(createdAt)
+  const { hourAndMinute } = useTimeAgo(date)
+  const { data } = useQuery(FIND_USER_BY_USER, { variables: { user: user } })
+
+  const navigatePost = () => {
+    data?.findUserById &&
+      navigation.navigate('DetailScreen', {
+        id: id,
+        randomColor: colorsRandom[Math.floor(Math.random() * colorsRandom.length)],
+        createdAt,
+        image,
+        title,
+        description,
+        user,
+        author,
+        name: data?.findUserById.me.name,
+        username: data?.findUserById.me.username,
+        verified: data?.findUserById.me.verified,
+        photo: data?.findUserById.me.photo,
+        descriptionUser: data?.findUserById.description,
+        location: data?.findUserById.location,
+        followers: data?.findUserById.followers,
+        following: data?.findUserById.following,
+        email: data?.findUserById.me.email,
+        website: data?.findUserById.website,
+        hourAndMinute,
+        bookUrl,
+        comments,
+        likes,
+      })
+  }
+
+  return (
+    <TouchableHighlight onPress={navigatePost} underlayColor={colors.colorUnderlay}>
+      <View style={styles.container}>
+        <View>
+          <Image style={styles.image} source={{ uri: image }} />
+        </View>
+        <View style={styles.TextAndLikes}>
+          <Text style={[styles.text, { color: colors.text }]}>{title}</Text>
+          <BtnLike id={id} likes={likes.length} />
+        </View>
+      </View>
+    </TouchableHighlight>
+  )
+}
+
+export default memo(AllPostRankItem)
+
+const styles = StyleSheet.create({
+  container: {
+    flexShrink: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    maxWidth: 195,
+    padding: 10,
+  },
+  image: {
+    borderRadius: 10,
+    height: 'auto',
+    aspectRatio: 9 / 13,
+    resizeMode: 'cover',
+    width: '100%',
+  },
+  text: {
+    fontSize: 15,
+    width: '75%',
+    textAlign: 'center',
+  },
+  TextAndLikes: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 4,
+    minHeight: 45,
+  },
+})
---
+++ components/Post/AllPostRanking.js
@@ -0,0 +1,128 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { RefreshControl, StyleSheet, ActivityIndicator } from 'react-native'
+import { useLazyQuery, useQuery } from '@apollo/client'
+import { useTheme } from '@react-navigation/native'
+import { FlatList } from 'react-native-gesture-handler'
+
+import { ALL_POSTS_COUNT, ALL_POST_RANKING } from '../../post/graphql-queries'
+import AllPostRankItem from './AllPostRankItem'
+
+const INITIAL_PAGE = 10
+const ITEM_HEIGHT = 270
+
+const renderItem = ({ item }) => (
+  <AllPostRankItem
+    bookUrl={item.bookUrl}
+    createdAt={item.createdAt}
+    image={item.image}
+    title={item.title}
+    comments={item.comments}
+    description={item.description}
+    id={item.id}
+    likes={item.likes}
+    tags={item.tags}
+    user={item.user}
+    author={item.author}
+  />
+)
+
+const keyExtractor = item => item.id.toString()
+
+const getItemLayout = (data, index) => ({
+  length: ITEM_HEIGHT,
+  offset: ITEM_HEIGHT * index,
+  index,
+})
+
+const AllPostRanking = () => {
+  const { colors } = useTheme()
+  const [isLoading, setIsLoading] = useState(true)
+  const [currentPage, setCurrentPage] = useState(INITIAL_PAGE)
+  const [refreshing, setRefreshing] = useState(false)
+  const [getAllPostRank, { data, refetch }] = useLazyQuery(ALL_POST_RANKING)
+  const { data: allPostsCount } = useQuery(ALL_POSTS_COUNT)
+
+  useEffect(() => {
+    let cleanup = true
+    if (cleanup) {
+      getAllPostRank({ variables: { pageSize: INITIAL_PAGE, skipValue: 0 } })
+    }
+    return () => (cleanup = false)
+  }, [getAllPostRank])
+
+  useEffect(() => {
+    let cleanup = true
+    if (cleanup) {
+      if (currentPage !== INITIAL_PAGE) {
+        refetch({ pageSize: currentPage, skipValue: 0 })
+      }
+    }
+    return () => (cleanup = false)
+  }, [currentPage, refetch])
+
+  const renderLoader = () => {
+    return (
+      isLoading && (
+        <ActivityIndicator
+          style={{ flex: 1, marginVertical: 16, width: '100%' }}
+          color={colors.colorThirdBlue}
+          size='small'
+        />
+      )
+    )
+  }
+  const loadMoreItem = useCallback(() => {
+    if (allPostsCount && allPostsCount?.postCount === data?.allPostRanking.length) {
+      return setIsLoading(false)
+    }
+    setCurrentPage(currentPage + INITIAL_PAGE)
+  }, [allPostsCount, currentPage, data?.allPostRanking.length])
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true)
+    await refetch({ pageSize: INITIAL_PAGE, skipValue: 0 })
+    setCurrentPage(INITIAL_PAGE)
+    setIsLoading(true)
+    setRefreshing(false)
+  }, [refetch])
+
+  return data?.allPostRanking ? (
+    <FlatList
+      data={data?.allPostRanking}
+      renderItem={renderItem}
+      keyExtractor={keyExtractor}
+      ListFooterComponent={renderLoader}
+      onEndReached={loadMoreItem}
+      getItemLayout={getItemLayout}
+      initialNumToRender={INITIAL_PAGE}
+      onEndReachedThreshold={0}
+      removeClippedSubviews={true}
+      numColumns={2}
+      columnWrapperStyle={styles.column}
+      refreshControl={
+        <RefreshControl
+          progressBackgroundColor={colors.primary}
+          refreshing={refreshing}
+          colors={[colors.colorThirdBlue]}
+          onRefresh={onRefresh}
+        />
+      }
+    />
+  ) : (
+    <ActivityIndicator
+      style={{ flex: 1, width: '100%', height: '100%' }}
+      color={colors.colorThirdBlue}
+      size='large'
+    />
+  )
+}
+
+export default AllPostRanking
+
+const styles = StyleSheet.create({
+  column: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    paddingHorizontal: 6,
+  },
+})
