88185e4a4d11521f59d070d83a2de7052abce5ef
Luis Gabriel <janco7249@gmail.com>
2022-03-20 23:31:01 -0400

    add  defatult colors of config

---
+++ config/colors.js
@@ -0,0 +1,42 @@
+export const colors = {
+  colorPrimary: '#192734',
+  colorSecondary: '#202e3a',
+  colorThirdBlue: '#0099ff',
+  colorThirdPurple: '#5046bb',
+  colorThirdYellow: '#ffc540',
+  colorFourthRed: '#ef4444',
+  colorUnderlay: '#0003',
+  colorLikeRed: '#ff1c85',
+  textWhite: '#F5FDFF',
+  TextGray: '#728493',
+  border: '#93a3b833',
+}
+
+export const colorsLight = {
+  colorPrimary: '#F5FDFF',
+  colorSecondary: '#e2e8f0',
+  colorThirdBlue: '#0099ff',
+  colorThirdPurple: '#5046bb',
+  colorThirdYellow: '#ffc540',
+  colorFourthRed: '#ef4444',
+  colorUnderlay: '#bae6fdaa',
+  colorLikeRed: '#ff1c85',
+  text: '#000',
+  TextGray: '#4D5B66',
+  border: '#64748b',
+}
+
+export const colorsRandom = [
+  '#2666CF',
+  '#8A39E1',
+  '#BB6464',
+  '#219F94',
+  '#F94892',
+  '#035397',
+  '#FFC600',
+  '#502064',
+  '#4A3F35',
+  '#BE3144',
+  '#21325E',
+  '#F76E11',
+]
