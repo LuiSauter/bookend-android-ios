1fdde10bf7662ca5d810916c6fbee3322d28fe3d
Luis Gabriel <janco7249@gmail.com>
2022-03-27 20:01:07 -0400

    [refactor] added Auth provider

--- App.js
+++ App.js
@@ -1,88 +1,12 @@
 import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { useTheme } from 'react-native-paper'
-
-import TabNavigator from './navigation/TabNavigator'
-import SettingScreen from './screens/setting/SettingScreen'
-import DetailScreen from './screens/DetailScreen'
-import UserScreen from './screens/UserScreen'
-import NameUser from './components/NameUser'
-import DisplayScreen from './screens/setting/DisplayScreen'
-import PdfScreen from './screens/PdfScreen'
-
-const Stack = createNativeStackNavigator()
+import { AuthProvider } from './context/AuthContext'
+import StackNavigator from './navigation/StackNavigator'
 
 const App = () => {
-  const { colors } = useTheme()
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name='TabNavigator'
-        component={TabNavigator}
-        options={{ headerShown: false, title: 'Inicio' }}
-      />
-      <Stack.Screen
-        name='SettingScreen'
-        component={SettingScreen}
-        options={() => ({
-          title: 'Ajustes',
-          headerStyle: { backgroundColor: colors.primary },
-          headerTitleStyle: { color: colors.text },
-          headerTintColor: colors.text,
-        })}
-      />
-      <Stack.Screen
-        name='DisplayScreen'
-        component={DisplayScreen}
-        options={() => ({
-          title: 'Pantalla y idiomas',
-          headerStyle: { backgroundColor: colors.primary },
-          headerTitleStyle: { color: colors.text },
-          headerTintColor: colors.text,
-        })}
-      />
-      <Stack.Screen
-        name='DetailScreen'
-        component={DetailScreen}
-        options={() => ({
-          title: 'Book',
-          headerStyle: {
-            backgroundColor: colors.primary,
-          },
-          headerTitleStyle: { color: colors.text },
-          headerTintColor: colors.text,
-        })}
-      />
-      <Stack.Screen
-        name='UserScreen'
-        component={UserScreen}
-        options={({ route }) => ({
-          title: '',
-          headerStyle: { backgroundColor: `rgb(${route.params.dominantColor})` },
-          headerTintColor: 'white',
-          headerTitle: props => (
-            <NameUser
-              {...props}
-              name={route.params.name}
-              verified={false}
-              fontSize={17}
-              color='white'
-            />
-          ),
-        })}
-      />
-      <Stack.Screen
-        name='PdfScreen'
-        component={PdfScreen}
-        options={() => ({
-          title: '',
-          headerTitleStyle: { color: colors.text },
-          headerTintColor: colors.textGray,
-          headerShown: true,
-          headerTransparent: true,
-        })}
-      />
-    </Stack.Navigator>
+    <AuthProvider>
+      <StackNavigator />
+    </AuthProvider>
   )
 }
 
